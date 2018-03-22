b74b0e41f1416974df7b53bf0ddfc02ae9c3f784
juanbulls <juanbulls@yahoo.com>
2018-03-22 18:50:37 -0500

    sensless
    BAsic example only to view if this colored the functions

---
+++ entrada.vb
@@ -0,0 +1,8 @@
+Public Class aSolas
+  Private Sub forma_Carga(ByVal sender As System.Object) Handless MyBase.Load
+    dim a as Integer
+    if a >0 Then
+      a = 1
+    End If
+  End Sub
+End Class
